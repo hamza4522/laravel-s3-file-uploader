dd18b55acf9f6b73861e73bed2d9fcfd1f122f6b
hamza ali <hamza.ali@hybytes.com>
2022-09-14 14:26:31 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
+
+Route::get('image-upload', [ImageController::class, 'index' ])->name('image.index');
+Route::post('image-upload', [ImageController::class, 'post' ])->name('image.upload');
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
